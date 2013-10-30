ae3953d25bf1513b6e438ea3aedfe83ba56796bc
Ricardo <marcorcp1@hotmail.com>
2013-10-29 20:50:10 -0500

    el secret token ya no es más un secreto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arcomega::Application.config.secret_token = '5b6e39cf1ff2ad01fe1d2067b007f2d42504d3f1b486a574480fac900ae58fde5bc01ac1922e60491dafb7f9cccbd440d4d86b3db058a50afd0c1b2da9e7eca8'
